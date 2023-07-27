9f773b08e0f661dc35d50b8333fffa611eae645a
Jerald Stephenson <gramcrackerj@hotmail.com>
2023-07-26 23:17:53 -0700

    moved brandText() to lib/utils, had to change lib/utils to a tsx file to allow for jsx. Added branding to temporary dashboard.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation'
 
-import { cn } from '@/lib/utils';
+import { cn, brandText } from '@/lib/utils';
 import {
   Code,
   ImageIcon,
@@ -68,26 +68,25 @@ const routes = [
 ];
 
 // applies branding to any string that contains 'ai'
-const brandText = (text: string) => {
+// const brandText = (text: string) => {
   
-  const pattern = /ai/gi;
-  let beforeAI = ""
-  let afterAI = ""
+//   const pattern = /ai/gi;
+//   let beforeAI = ""
+//   let afterAI = ""
 
-  const match = pattern.exec(text);
-  if (match) {
-    beforeAI = text.slice(0, match.index);
-    afterAI = text.slice(match.index + match[0].length);
+//   const match = pattern.exec(text);
+//   if (match) {
+//     beforeAI = text.slice(0, match.index);
+//     afterAI = text.slice(match.index + match[0].length);
 
-    return (
-    <span className='group'>{beforeAI}<span className={cn('text-sky-500  group-hover:text-sky-400', sourceCodeProFont.className)}>AI</span>{afterAI}</span>
-      )
-  }
+//     return (
+//     <span className='group'>{beforeAI}<span className={cn('text-sky-500  group-hover:text-sky-400', sourceCodeProFont.className)}>AI</span>{afterAI}</span>
+//       )
+//   }
  
-  return text
+//   return text
    
-   
-};
+// };
 
 const Sidebar = () => { 
   const pathname = usePathname();
---
+++ lib/utils.tsx
@@ -0,0 +1,34 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import { Source_Code_Pro } from 'next/font/google';
+
+ function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+const sourceCodeProFont = Source_Code_Pro({
+  weight: '600',
+  subsets: ['latin'],
+});
+
+// applies branding to any string that contains 'ai'
+ const brandText = (text: string) => {
+  
+  const pattern = /ai/gi;
+  let beforeAI = ""
+  let afterAI = ""
+
+  const match = pattern.exec(text);
+  if (match) {
+    beforeAI = text.slice(0, match.index);
+    afterAI = text.slice(match.index + match[0].length);
+
+    return (
+      <span className='group'>{beforeAI}<span className={cn('text-sky-500  group-hover:text-sky-400', sourceCodeProFont.className)}>AI</span>{afterAI}</span>
+        )
+  }
+  return text
+};
+ 
+export {cn, brandText}
