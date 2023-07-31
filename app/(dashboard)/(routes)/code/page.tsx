ac4d6a5d175a5ac6c522ef0241aeb7eee55b3f5c
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-07-30 18:12:44 -0700

    formatted code generator responses

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -11,6 +11,7 @@ import { ChatCompletionRequestMessage } from 'openai';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import ReactMarkdown from 'react-markdown';
 
 import { formSchema } from './constants';
 import { brandText, cn } from '@/lib/utils';
@@ -121,7 +122,21 @@ const CodePage = () => {
                 )}
               >
                 {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                <p className='text-sm'>{message.content}</p>
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className='overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg'>
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className='bg-black/10 rounded-lg p-1' {...props} />
+                    ),
+                  }}
+                  className='text-sm overflow-hidden leading-7'
+                >
+                  {message.content || ''}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
