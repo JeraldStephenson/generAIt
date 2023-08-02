c7b1cf99dfe4c795dd4d32df11ecabc678e3cb7b
Jerald Stephenson <gramcrackerj@hotmail.com>
2023-08-02 11:26:52 -0700

    gloabal constants file created to track global variables, api-limit utils functions added

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,6 +4,7 @@ declare global {
     var prisma: PrismaClient | undefined;
 };
 
+// to help prevent multiple instance of PrismaClient with next.js 13 hot reloading
 const prismadb = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
