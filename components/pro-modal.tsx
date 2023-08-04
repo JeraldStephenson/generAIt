11967dfedb6b57fdad178c738b940f741bcfbdaf
Jerald Stephenson <gramcrackerj@hotmail.com>
2023-08-03 19:46:26 -0700

    added styled badge to pro-modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -8,6 +8,7 @@ import {
  } from "@/components/ui/dialog"
 import { useProModal } from "@/hooks/use-pro-modal"
 import { brandText } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
 
 export const ProModal = () => {
     const proModal = useProModal()
@@ -17,7 +18,12 @@ export const ProModal = () => {
         <DialogContent>
             <DialogHeader>
                 <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                <div className="flex items-center gap-x-2 font-bold py-1">
                    <p className="tracking-wide"> Upgrade to {brandText('Generait')} Unlimited</p>
+                   <Badge variant={'premium'} className="uppercase text-sm py-1 ">
+                    pro
+                   </Badge>
+                </div>
                 </DialogTitle>
             </DialogHeader>
         </DialogContent>
