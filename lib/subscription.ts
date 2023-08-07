5d6f2cafe6bca9273c00a85545fc7bb6791b8fec
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-08-07 15:16:20 -0700

    util function to check if user is currently subscribed

---
+++ lib/subscription.ts
@@ -0,0 +1,37 @@
+import { auth } from '@clerk/nextjs';
+import prismadb from '@/lib/prismadb';
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return false;
+  }
+
+  const userSubscription = await prismadb.userSubscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      stripeSubscriptionId: true,
+      stripeCurrentPeriodEnd: true,
+      stripeCustomerId: true,
+      stripePriceId: true,
+    },
+  });
+
+  if (!userSubscription) {
+    return false;
+  }
+
+  const isValid =
+    userSubscription.stripePriceId &&
+    // one day grace period
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
+
+  // !! is to make sure isValid is a boolean
+  return !!isValid;
+};
