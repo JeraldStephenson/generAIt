49c2749c2708693738ee92df22c85822b7ac7b69
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-08-08 11:25:21 -0700

    replaced custom error handling in pro-modal and sub-button components with toast error handling

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -3,6 +3,7 @@
 import { Check, Zap } from 'lucide-react';
 import { useState } from 'react';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 import { featureList } from '@/lib/features-list-styles';
 import { useProModal } from '@/hooks/use-pro-modal';
@@ -30,7 +31,7 @@ export const ProModal = () => {
       const response = await axios.get('/api/stripe');
       window.location.href = response.data.url;
     } catch (error) {
-      console.log(error, 'STRIPE_CLIENT_ERROR');
+      toast.error('Something went wrong.');
     } finally {
       setLoading(false);
     }
