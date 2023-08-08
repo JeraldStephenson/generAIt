f0f75e3e308a99fd755687f3be65e0ef36a42582
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-08-08 11:01:00 -0700

    upgrade to premium modal should now only pop up if on free tier and at max free uses

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,11 +1,12 @@
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
-import Replicate from "replicate";
+import Replicate from 'replicate';
 
 import { increaseApiLimit, checkApiLimit } from '@/lib/api-limit';
+import { checkSubscription } from '@/lib/subscription';
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!
+  auth: process.env.REPLICATE_API_TOKEN!,
 });
 
 export async function POST(req: Request) {
@@ -18,28 +19,29 @@ export async function POST(req: Request) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
-
     if (!prompt) {
       return new NextResponse('Prompt is required', { status: 400 });
     }
 
     const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
 
-    if (!freeTrial) {
-      return new NextResponse("Free trial has expired.", {status: 403});
+    if (!freeTrial && !isPro) {
+      return new NextResponse('Free trial has expired.', { status: 403 });
     }
 
-
     const response = await replicate.run(
-      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      'anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351',
       {
         input: {
-          prompt: prompt
-        }
+          prompt: prompt,
+        },
       }
     );
 
-    await increaseApiLimit();
+    if (!isPro) {
+      await increaseApiLimit();
+    }
 
     return NextResponse.json(response);
   } catch (error) {
